aa3d86981ba55d8e9fc37440e9653261b6fb2fc1
mika0616 <nokolight0616@gmail.com>
2020-01-12 19:11:10 +0900

    [finish]3-7編集機能を追加

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -6,6 +6,7 @@ class TodolistsController < ApplicationController
 
   def create
   	# ストロングパラメータ(list_params)を使用
+  	# 表示の工程がないため、view用のインスタント変数は必要ない
     list =List.new(list_params)
   	# ｄｂへList.new(list_params)を保存
     list.save
@@ -19,6 +20,18 @@ class TodolistsController < ApplicationController
 
   def show
   	@list =List.find(params[:id])
+  end
+
+  def edit
+  	@list =List.find(params[:id])
+  end
+
+  def update
+  	# showアクションにリダイレクトするため、新たなviewがいらない
+  	# また、表示の工程が必要ないため、インスタント変数指定しなくていい。
+  	list =List.find(params[:id])
+  	list.update(list_params)
+  	redirect_to todolist_path(list.id)
   end
 
   # ストロングパラメータの境界線（この下には何も書かない）
--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,21 @@ Rails.application.routes.draw do
   # /topというURLにgetメソッドでリクエストしたときに
   # homeコントローラーにてtopアクションが呼び出される
   get 'top' => 'homes#top'
-  
+
+# 投稿を新規作成
   # /todolists というURLにpostメソッドでリクエストしたときに、
   # todolistsコントローラーにcreateアクションが呼び出される
   post 'todolists' => 'todolists#create'
 
+# 一覧を表示
   get 'todolists' => 'todolists#index'
 
+# 投稿の詳細ページにアクセス
 # 名前付きルートを定義　:以下はidを指定
 # todolist#showの設定をtodolistとして利用できる
   get 'todolists/:id' =>'todolists#show', as: 'todolist'
+
+# 編集機能を追加
+  get 'todolists/:id/edit' =>'todolists#edit', as:'edit_todolist'
+  patch 'todolists/:id' => 'todolists#update', as:'update_todolist'
 end
