b1cb67f51dc7bc870cd84f7a8c8bb067e66530c9
mika0616 <nokolight0616@gmail.com>
2020-01-13 15:48:10 +0900

    [finish]3-8画像投稿機能を実装

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+#refile使用のため以下シークレットキーを追記
+Refile.secret_key = '34b3188ebc243e3dfd9deb17b8f65acf4f6b92bb1015e50a7904579f9558739de40c30c6a2f89a1390598c1632d26f0a6162bce3a836cd60db742429934acc3d'
